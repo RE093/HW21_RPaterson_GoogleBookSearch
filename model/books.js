cbd0b9746969ed06be24bafee941a1424e6b234d
RE000 <re.paterson000@gmail.com>
2020-09-28 12:31:15 +1000

    created book model

---
+++ model/books.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: {
+        type: String,
+    },
+    authors: [
+        {
+            type: String,
+        },
+    ],
+    description: {
+        type: String,
+    },
+    image: {
+        data: Buffer,
+        contentType: String,
+    },
+});
+
+const Book = mongoose.model("Book", BookSchema);
+module.exports = Book;
