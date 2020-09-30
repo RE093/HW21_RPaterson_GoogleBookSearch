a304af637912250fac50be45fedd76f3757f082f
RE000 <re.paterson000@gmail.com>
2020-10-01 08:14:01 +1000

    heroku troubleshooting p2

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -23,14 +19,7 @@ mongoose.connect(
   useFindAndModify: false
 });
 
-// Define API routes here
 require("./controller/book-routes.js")(app);
-
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./view/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
