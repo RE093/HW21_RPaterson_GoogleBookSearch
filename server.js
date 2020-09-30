a76faa94a0d67763f5a231a0bbefe148d7ceb393
RE000 <re.paterson000@gmail.com>
2020-10-01 07:20:58 +1000

    deploying

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ if (process.env.NODE_ENV === "production") {
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { 
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // Define API routes here
